9787f9d624e10ffa28f7bd90f6c6974c98d48608
david25ajayi-art <david25ajayi@gmail.com>
2026-08-23 04:42:57 +0100

    Refactor search handler to use URL API

--- api/search.js
+++ api/search.js
@@ -1,10 +1,17 @@
-export default async function handler(req, res) {
-    const query = req.query.query;
+export default async function handler(request) {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("query");
 
     if (!query) {
-        return res.status(400).json({
-            error: "Search query is required"
-        });
+        return new Response(
+            JSON.stringify({ error: "Search query is required" }),
+            {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
     }
 
     const apiKey = process.env.WEATHER_API_KEY;
@@ -15,11 +22,22 @@ export default async function handler(req, res) {
         const response = await fetch(url);
         const data = await response.json();
 
-        return res.status(response.status).json(data);
+        return new Response(JSON.stringify(data), {
+            status: response.status,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
 
     } catch (error) {
-        return res.status(500).json({
-            error: "Something went wrong"
-        });
+        return new Response(
+            JSON.stringify({ error: "Something went wrong" }),
+            {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
     }
 }
